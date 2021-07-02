075d308ff64c8cff9677f8fa05ced88241e904e1
ryanmcd96 <ryanmcdonald9685@gmail.com>
2021-07-02 04:44:20 +0100

    firebase setup

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAZN5o0SBtAj3mc9H2fMqBvc6TgTFGBxqo",
+  authDomain: "react-chat-a33cd.firebaseapp.com",
+  projectId: "react-chat-a33cd",
+  storageBucket: "react-chat-a33cd.appspot.com",
+  messagingSenderId: "16018689511",
+  appId: "1:16018689511:web:3de256ab62e3d6ffc1c142"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
