d9836b7c1c571d75a858640da6ecdf7706f4d91d
ryanmcd96 <ryanmcdonald9685@gmail.com>
2021-07-03 12:14:31 +0100

    reading from the firestore db

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -7,6 +7,7 @@ import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 firebase.initializeApp({
   apiKey: "AIzaSyAZN5o0SBtAj3mc9H2fMqBvc6TgTFGBxqo",
@@ -21,13 +22,78 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
       
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}> Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+
+  )
+}
+
+function ChatRoom() {
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+
+  return (<>
+    <div>
+    
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+    </div>
+
+    <form onSubmit={sendMessage}>
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+
+      <button type="submit">🕊️</button>
+
+    </form>
+    </>)
+}
+
+function ChatMessage(props) {
+  const { text, uid} = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+ return (
+   <div className={`message ${messageClass}`}>
+     <img src={photoURL} />
+ <p>{text}</p>
+ </div>
+
+ ) 
+}
+
+
 
 export default App;
